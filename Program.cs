6aceb98644c96f5dbc3817395736bdf20639334e
FIC <fic@info.local>
2025-03-27 21:52:24 -0300

    Heranca

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿//Heranca - Uma classe pode pegar todos os atributos e metodos de outra classe
+
+//Carro - Modelo, Marca, Quilometragem
+//CarroEletrico - Bateria, TempoDeCarga
+//CarroCombustao - Combustivel
+
+//
+
+//using Heranca;
+
+//CarroEletrico carroE = new CarroEletrico();
+//carroE.marca = "BYD";
+//carroE.bateria = 200;
+
+//CarroCombustao carroC = new CarroCombustao();
+//carroC.marca = "Peujeot";
+//carroC.combustivel = "Diesel";
+
+
+//Carro meuCarro;
+
+//Console.WriteLine("Digite E - para ELETRICO ou Digite C - para Combustao");
+//string opcao = Console.ReadLine();
+
+//if (opcao == "E")
+//{
+//    meuCarro = new CarroEletrico();
+//}
+//else if (opcao == "C")
+//{
+//    meuCarro = new CarroCombustao();
+//}
+
+using Heranca;
+
+PessoaF pFis = new PessoaF();
+pFis.nome = "Jurandir";
+pFis.CPF = "40028922";
+
+PessoaJ pJur = new PessoaJ();
+pJur.nome = "YudiPLAYStory";
+pJur.CNPJ = "741258963/0002-84";
+
+
+
+
+
+
+
+
+
+
+
+
